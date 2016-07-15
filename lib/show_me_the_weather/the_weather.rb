1c827f145d4465825e5db32e829e67e84d707dd7
Ricardo Ide <ryiskate@yahoo.com.br>
2016-07-15 21:46:00 +0000

    using opem weather

--- lib/show_me_the_weather/the_weather.rb
+++ lib/show_me_the_weather/the_weather.rb
@@ -1,7 +1,16 @@
+require 'rest-client'
+require 'json'
+
 module ShowMeTheWeather
   class TheWeather
-    def self.the_weather
-      "test"
+
+    def initialize
+      @host = 'http://api.openweathermap.org/data/2.5'
+    end
+    
+    def the_weather(city)
+      response = RestClient.get "#{@host}/weather", {:params => {q: city, appid: ENV['OPEN_WEATHER_APPID'], units: 'metric'}}
+      JSON.parse(response.body)
     end
   end
 end
